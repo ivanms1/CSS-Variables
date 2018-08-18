a15af654b47fb845ce1a0093ddfed3c8b7e1f187
Ivan S <ivanms1@gmail.com>
2018-08-18 19:22:55 +0700

    added materialize css

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const inputs = document.querySelectorAll('.controls input')
+const inputs = document.querySelectorAll('.controls input');
 
-function handleUpdate(){
-	let suffix = this.dataset.sizing || '';
+function handleChange(){
+	const suffix = this.dataset.sizing || '';
 	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
-;}
+}
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate))
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
+inputs.forEach(input => input.addEventListener('change', handleChange));
+inputs.forEach(input => input.addEventListener('mousemove', handleChange));
